5ea28a943b36194aa3c6a37f2f85f938f79b54d7
Mitch Cairns <mitch.cairns@handheldlegend.com>
2023-06-14 16:43:58 -0700

    Small include change

--- src/phob_includes.h
+++ src/phob_includes.h
@@ -10,16 +10,11 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-#include "hardware/gpio.h"
-#include "hardware/pwm.h"
-
 #include "curve_fitting.h"
 #include "stick_cal.h"
+#include "filter.h"
 
 #include "phob_board_config.h"
 #include "phob_types.h"
-
-#include "hardware/pio.h"
-#include "joybus.pio.h"
 
 #endif
